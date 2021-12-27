58383fc3f52b2a4efa75f0cfc3995ee6324eeae5
jindex2411 <js.jeong@linewalks.com>
2021-12-27 14:59:09 +0900

    feat:add add_queue_binding func

--- dasima/exchange.py
+++ dasima/exchange.py
@@ -1,4 +1,4 @@
-from kombu import Exchange, Queue
+from kombu import Exchange, Queue, binding
 from flask.ctx import AppContext
 from dasima.worker import Worker
 
@@ -19,6 +19,7 @@ class ExchangeWrapper:
         type=exchange_type,
         durable=True
     )
+    self.binding_dict = {}
 
   def send_message(self, data, routing_key):
     print("__exchangerwrapper_send_message__")
@@ -28,33 +29,47 @@ class ExchangeWrapper:
         routing_key
     )
 
-  def subscribe(self, routing_key):
+  # def subscribe(self, routing_key):
+  #   print(f"__exchangerwrapper_subscribe_{routing_key}__")
+  #   def decorator(func):
+  #     self.add_consumer_config(func, routing_key)
+  #     return func
+  #   return decorator
+  
+  def subscribe(self, queue, routing_key):
     print(f"__exchangerwrapper_subscribe_{routing_key}__")
     def decorator(func):
-      self.add_consumer_config(func, routing_key)
+      self.add_queue_binding(queue, routing_key, func)
       return func
     return decorator
-
-  def add_consumer_config(
-      self,
-      func,
-      routing_key
-  ):
+  
+  def add_queue_binding(self, queue, routing_key, func):
     print("__exchangerwrapper_add_consumer__")
-    queue_name = func.__name__
-    queue = Queue(
-        name=queue_name,
-        exchange=self.exchange,
-        routing_key=routing_key,
-        durable=True
-    )
+    if self.binding_dict.get(queue):
+      self.binding_dict[queue].append((routing_key, func))
+    else:
+      self.binding_dict[queue] = [(routing_key, func)]
+    
+  # def add_consumer_config(
+  #     self,
+  #     func,
+  #     routing_key
+  # ):
+  #   print("__exchangerwrapper_add_consumer__")
+  #   queue_name = func.__name__
+  #   queue = Queue(
+  #       name=queue_name,
+  #       exchange=self.exchange,
+  #       routing_key=routing_key,
+  #       durable=True
+  #   )
 
-    def on_task(body, message):
-      try:
-        self.app_ctx.push()
-        func(**body)
-      finally:
-        message.ack()
-        self.app_ctx.pop()
+  #   def on_task(body, message):
+  #     try:
+  #       self.app_ctx.push()
+  #       func(**body)
+  #     finally:
+  #       message.ack()
+  #       self.app_ctx.pop()
 
-    self.worker.add_consumer_config(queue, on_task)
+  #   self.worker.add_consumer_config(queue, on_task)
--- example/main/test_functions.py
+++ example/main/test_functions.py
@@ -2,12 +2,12 @@ from flask import current_app as app
 from main import dasimamq
 
 
-@dasimamq.clue.subscribe("info")
-def setting_info():
-  print("Dasima setting parameters")
-  print("DASIMA_CONNECTION_HOST: ", app.config["DASIMA_CONNECTION_HOST"])
-  print("DASIMA_ACCEPT_TYPE: ", app.config["DASIMA_ACCEPT_TYPE"])
-  print("DASIMA_EXCHANGE_SETTING: ", app.config["DASIMA_EXCHANGE_SETTING"])
+# @dasimamq.clue.subscribe("info")
+# def setting_info():
+#   print("Dasima setting parameters")
+#   print("DASIMA_CONNECTION_HOST: ", app.config["DASIMA_CONNECTION_HOST"])
+#   print("DASIMA_ACCEPT_TYPE: ", app.config["DASIMA_ACCEPT_TYPE"])
+#   print("DASIMA_EXCHANGE_SETTING: ", app.config["DASIMA_EXCHANGE_SETTING"])
 
 
 @dasimamq.clue.subscribe("add")
@@ -16,19 +16,19 @@ def add(x, y):
   return x + y
 
 
-@dasimamq.clue.subscribe("mul")
-def mul(x, y):
-  print("MUL", x, y)
-  return x * y
+# @dasimamq.clue.subscribe("mul")
+# def mul(x, y):
+#   print("MUL", x, y)
+#   return x * y
 
 
-@dasimamq.login.subscribe("div")
-def div(x, y):
-  print("DIV", x, y)
-  return x // y
+# @dasimamq.login.subscribe("div")
+# def div(x, y):
+#   print("DIV", x, y)
+#   return x // y
 
 
-@dasimamq.login.subscribe("sub")
-def sub(x, y):
-  print("SUB", x, y)
-  return x - y
+# @dasimamq.login.subscribe("sub")
+# def sub(x, y):
+#   print("SUB", x, y)
+#   return x - y
