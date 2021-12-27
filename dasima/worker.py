54c4119ab5e76f89132760ec51a4d20b6078d2ee
jindex2411 <js.jeong@linewalks.com>
2021-12-27 15:42:24 +0900

    feat: add worker add_consumer_config_list func

--- dasima/worker.py
+++ dasima/worker.py
@@ -1,4 +1,4 @@
-from kombu import Connection, Consumer
+from kombu import Connection, Consumer, Queue, binding
 from kombu.mixins import ConsumerProducerMixin
 
 
@@ -29,6 +29,41 @@ class Worker(ConsumerProducerMixin):
       # TODO maybe_close_channel
       if channel:
         channel.close()
+  
+  def make_func(self, func_list):
+    def func(data, key):
+      func_dict = dict(func_list)
+      if func_dict.get(key):
+        return func_dict[key](**data)
+      return "error"
+    return func
+
+  def add_consumer_config_list(self, exchangewrapper):
+    func_dict = exchangewrapper.func_dict
+
+    for key, value in func_dict.items():
+      func = self.make_func(value)
+      bind_list = [
+          binding(exchangewrapper.exchange, routing_key=routing_key)
+          for routing_key, _ in value
+      ]
+      def on_task(body, message):
+        try:
+          self.app_ctx.push()
+          func(**body)
+        finally:
+          message.ack()
+          self.app_ctx.pop()
+      Queue(
+        name=key,
+        exchange=exchangewrapper.exchange,
+        bindings=bind_list,
+        durable=True
+      )
+      self.add_consumer_config.append(queue, on_task)
+      
+
+
 
   # kombu의 각각에 Channel에 독립적인 threading을 적용 하기 전
   # 사전 작업 으로 각자의 Consumer마다 channel을 할당
